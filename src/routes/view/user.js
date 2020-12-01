a62a968cf7fa3488be8ddfa6181cf2491351481d
lhj1101 <751609117@qq.com>
2020-12-01 17:35:43 +0800

    feat: 判断已登录状态

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -5,14 +5,36 @@
 
 const router = require('koa-router')()
 
+/**
+ * 获取登录信息
+ * @param {Object} ctx ctx
+ */
+function getLoginInfo (ctx) {
+  let data = {
+    isLogin: false  // 未登录
+  }
+  const userInfo = ctx.session.userInfo
+  if (userInfo) {
+    data = {
+      isLogin: true,
+      userName: userInfo.userName
+    }
+    console.log(data)
+    console.log(data.isLogin)
+    console.log(data.userName)
+  }
+  return data
+}
+
 // 登录页
+// ctx.render('login', {参数})
 router.get('/login', async (ctx, next) => {
-  await ctx.render('login', {})
+  await ctx.render('login', getLoginInfo(ctx))
 })
 
 // 注册页
 router.get('/register', async (ctx, next) => {
-  await ctx.render('register', {})
+  await ctx.render('register', getLoginInfo(ctx))
 })
 
 module.exports = router

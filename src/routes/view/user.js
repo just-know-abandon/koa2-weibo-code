bf2c4ac853944899011b72a399e681583361470d
lhj1101 <751609117@qq.com>
2020-12-02 15:18:44 +0800

    feat: setting界面

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -4,6 +4,7 @@
  */
 
 const router = require('koa-router')()
+const { loginRedirect } = require('../../middlewares/loginCheck')
 
 /**
  * 获取登录信息
@@ -35,6 +36,11 @@ router.get('/login', async (ctx, next) => {
 // 注册页
 router.get('/register', async (ctx, next) => {
   await ctx.render('register', getLoginInfo(ctx))
+})
+
+// 设置页
+router.get('/setting', loginRedirect, async (ctx, next) => {
+  await ctx.render('setting', ctx.session.userInfo)
 })
 
 module.exports = router
